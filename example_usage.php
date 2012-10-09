f312181e6aad0913ea52e2dc9f3280dea46af626
Chris Bake <cbake7806@gmail.com>
2012-10-09 14:59:23 -0500

    debug

---
+++ example_usage.php
@@ -0,0 +1,44 @@
+kohana-pagination-directories
+=============================
+
+Kohana Pagination support for Routes using directories
+
+
+Example Controller Usage
+
+<?php
+$pagination = Pagination::factory(array(
+    'total_items'    => Model_Photos::count('member', MEMBER_ID, true),
+    'items_per_page' => 14,
+    'view'			 => 'pagination/basic',
+    'auto_hide'		 => TRUE,
+    'current_page'	 => array(
+	    "key"		=> "page",
+	    "directory"		=> $this->request->directory(),
+	    "controller"	=> $this->request->controller(),
+	    "action"		=> $this->request->action(),
+	    "source"		=> "directory_route",
+    ),
+));
+
+$page_links = $pagination->render();
+
+?>
+
+Example Route Support
+
+
+<?php
+
+Route::set('members_pagination', '<directory>(/<controller>(/<action>(/<page>)))', array(
+		'directory'	=> '(members)',
+		'controller'	=> '(photos|videos|articles)',
+		'action'	=> '(index)',
+	))
+	->defaults(array(
+	    'directory'  => 'members',
+	    'controller' => 'home',
+	    'action'     => 'index',
+	    'page'	 => 1,
+	));
+?.
